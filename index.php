13cc020e076e75758a881afd8fe0db49e50ed678
yustianbudiman <blog3489@gmail.com>
2018-07-13 11:13:40 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+									<table id="tableAppPendukung"
+										class="table table-striped table-bordered table-hover">
+										<thead>
+											<tr>
+												<th>No</th>
+												<th>Nama Aplikasi</th>
+												<th>URL Aplikasi</th>
+												<th>Parameter Redirect</th>
+												<th>Parameter Redirect2</th>
+												<th>Parameter Redirect3</th>
+												<th style="text-align: center;">Action</th>
+											</tr>
+										</thead>
+										<tbody id="tableAppPendukungBody">
+											
+										</tbody>
+									</table>
