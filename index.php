826e9b53955d9d28548b178f23924fba2aab9cb5
yustianbudiman <blog3489@gmail.com>
2018-07-13 11:16:07 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,22 @@
-									<table id="tableAppPendukung"
-										class="table table-striped table-bordered table-hover">
-										<thead>
-											<tr>
-												<th>No</th>
-												<th>Nama Aplikasi</th>
-												<th>URL Aplikasi</th>
-												<th>Parameter Redirect</th>
-												<th>Parameter Redirect2</th>
-												<th>Parameter Redirect3</th>
-												<th style="text-align: center;">Action</th>
-											</tr>
-										</thead>
-										<tbody id="tableAppPendukungBody">
-											
-										</tbody>
-									</table>
+<script src="https://code.jquery.com/jquery-3.3.1.js" type="text/javascript"></script>
+
+<script src="https://cdn.datatables.net/v/dt/dt-1.10.18/datatables.min.js"></script>
+<script src="js_datatable.js"></script>
+
+<table id="tableAppPendukung"
+	class="table table-striped table-bordered table-hover">
+	<thead>
+		<tr>
+			<th>No</th>
+			<th>Nama Aplikasi</th>
+			<th>URL Aplikasi</th>
+			<th>Parameter Redirect</th>
+			<th>Parameter Redirect2</th>
+			<th>Parameter Redirect3</th>
+			<th style="text-align: center;">Action</th>
+		</tr>
+	</thead>
+	<tbody id="tableAppPendukungBody">
+
+	</tbody>
+</table>
